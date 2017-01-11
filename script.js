40374e4ff5f142b4a088cc88a62ac022da786e40
Damare Jerome <damaretheboy@gmail.com>
2017-01-11 14:59:22 +0000

    update

---
+++ script.js
@@ -0,0 +1,33 @@
+TinyTurtle.apply(window); 
+
+    forward(50);
+
+left(90);
+
+    forward(50);
+
+left(90);
+
+    forward(50);
+
+left(90);
+
+    forward(50);
+    
+left(90);
+    
+    forward(50);
+    
+left(45); 
+
+    forward(30);
+        
+left(80);
+
+    forward(36);
+
+
+  
+    
+
+
